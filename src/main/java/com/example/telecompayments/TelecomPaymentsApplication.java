3c04b37a42447be605a6872c36769abaee37d309
manish9427 <manish119427@gmail.com>
2025-07-07 12:38:27 +0530

    first commit

---
+++ src/main/java/com/example/telecompayments/TelecomPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.telecompayments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelecomPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelecomPaymentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/telecompayments/TelecomPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telecompayments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelecomPaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
